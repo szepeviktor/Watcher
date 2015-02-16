1292639c2526598d9a47689cfb28928191cfd2ef
Oleg Palij <o.palij@gmail.com>
2015-02-16 21:17:31 +0200

    removed video extensions

--- watcher.py
+++ watcher.py
@@ -15,19 +15,6 @@ import logging, time
 import daemon, lockfile
 from lockfile import pidlockfile
 
-#third party libs
-#: Video extensions
-try:
-    from subliminal import VIDEO_EXTENSIONS
-except ImportError:
-    VIDEO_EXTENSIONS = ('.3g2', '.3gp', '.3gp2', '.3gpp', '.60d', '.ajp', '.asf', '.asx', '.avchd', '.avi', '.bik',
-                        '.bix', '.box', '.cam', '.dat', '.divx', '.dmf', '.dv', '.dvr-ms', '.evo', '.flc', '.fli',
-                        '.flic', '.flv', '.flx', '.gvi', '.gvp', '.h264', '.m1v', '.m2p', '.m2ts', '.m2v', '.m4e',
-                        '.m4v', '.mjp', '.mjpeg', '.mjpg', '.mkv', '.moov', '.mov', '.movhd', '.movie', '.movx', '.mp4',
-                        '.mpe', '.mpeg', '.mpg', '.mpv', '.mpv2', '.mxf', '.nsv', '.nut', '.ogg', '.ogm', '.omf', '.ps',
-                        '.qt', '.ram', '.rm', '.rmvb', '.swf', '.ts', '.vfw', '.vid', '.video', '.viv', '.vivo', '.vob',
-                        '.vro', '.wm', '.wmv', '.wmx', '.wrap', '.wvx', '.wx', '.x264', '.xvid')
-
 
 class DaemonRunnerError(Exception):
     """ Abstract base class for errors from DaemonRunner. """
@@ -290,11 +277,6 @@ def watcher(config):
 
         logger.info(section + ": " + folder)
 
-        # parse include_extensions
-        if include_extensions and 'video' in include_extensions:
-            include_extensions.discard('video')
-            include_extensions |= set(VIDEO_EXTENSIONS)
-      
         wm = pyinotify.WatchManager()
         handler = EventHandler(command, include_extensions, exclude_extensions)
 
